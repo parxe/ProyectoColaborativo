b6dcce33e64c7c5493c930455c735f4cc0558763
parxe <130541259+parxe@users.noreply.github.com>
2024-09-30 11:56:54 +0200

    commit

--- GithubP.py
+++ GithubP.py
@@ -1,9 +1,9 @@
 for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
-        print("Fizz")
+        print("FizzBuzz")
     elif i % 3 == 0:
-        print("Coca")
+        print("Fizz")
     elif i % 5 == 0:
-        print("Cola")
+        print("Buzz")
     else:
         print(i)
